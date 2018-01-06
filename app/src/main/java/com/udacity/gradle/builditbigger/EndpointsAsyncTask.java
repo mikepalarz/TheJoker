acaa94a90a976b5b65dced2205437abf451ef708
Mike Palarz <mpalarz1@gmail.com>
2018-01-06 11:22:43 -0600

    feat: Adjusted EndpointsAsyncTask to be its own class
    Adjusted the EndpointsAsyncTask to be a class of its own. This
    was driven per a recommendation given by a mentor at Udacity
    to have the EndpointsAsyncTask have an interface and for the
    activity to implement the interface. This approach keeps things
    modular. The discussion that inspired this approach can be
    found here:
    https://discussions.udacity.com/t/modifying-gce-starter-code-and-creating-async-task/473168/9

---
+++ app/src/main/java/com/udacity/gradle/builditbigger/EndpointsAsyncTask.java
@@ -0,0 +1,68 @@
+package com.udacity.gradle.builditbigger;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
+
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
+import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
+import com.palarz.mike.jokedisplayer.JokeDisplayer;
+import com.udacity.gradle.builditbigger.backend.myApi.MyApi;
+
+import java.io.IOException;
+
+/**
+ * Created by mpala on 1/5/2018.
+ */
+
+class EndpointsAsyncTask extends AsyncTask<Context, Void, String> {
+
+    interface PostExecuteCallback {
+        String supplyJoke(String theJoke);
+    }
+
+    private static MyApi myApiService = null;
+    private Context context;
+
+    private static PostExecuteCallback mCallback = null;
+
+    @Override
+    protected String doInBackground(Context... params) {
+        if(myApiService == null) {  // Only do this once
+            MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
+                    new AndroidJsonFactory(), null)
+                    // options for running against local devappserver
+                    // - 10.0.2.2 is localhost's IP address in Android emulator
+                    // - turn off compression when running against local devappserver
+                    .setRootUrl("http://10.0.2.2:8080/_ah/api/")
+                    .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
+                        @Override
+                        public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
+                            abstractGoogleClientRequest.setDisableGZipContent(true);
+                        }
+                    });
+            // end options for devappserver
+
+            myApiService = builder.build();
+        }
+
+        context = params[0];
+
+        if (mCallback == null) {
+            mCallback = (PostExecuteCallback) context;
+        }
+
+        try {
+            return myApiService.tellJoke().execute().getJoke();
+        } catch (IOException e) {
+            return e.getMessage();
+        }
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        mCallback.supplyJoke(result);
+    }
+}
